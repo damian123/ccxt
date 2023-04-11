6be314d4742979b749cb1fabb0f0eb12da0f5f6b
Damian <727816+damian123@users.noreply.github.com>
2023-04-11 18:16:40 +0900

    Add Windows CI

--- scripts/compile/build_debug.bat
+++ scripts/compile/build_debug.bat
@@ -2,6 +2,5 @@
 
 cmake -E remove_directory vs_studio_build
 if not exist build mkdir vs_studio_build
-REM --debug-output
-cmake -B vs_studio_build -S . -GNinja -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_TOOLCHAIN_FILE=d:\dev\trading\vcpkg\scripts\buildsystems\vcpkg.cmake
-cmake --build vs_studio_build
+cmake -B vs_studio_build -S . -GNinja -DCMAKE_TOOLCHAIN_FILE=d:\dev\trading\vcpkg\scripts\buildsystems\vcpkg.cmake
+cmake --build vs_studio_build --config Debug
--- scripts/compile/build_vs_debug.bat
+++ scripts/compile/build_vs_debug.bat
@@ -2,7 +2,6 @@
 
 cmake -E remove_directory vs_studio_build
 if not exist build mkdir vs_studio_build
-REM --debug-output
-cmake -B vs_studio_build -S . -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_TOOLCHAIN_FILE=d:\dev\trading\vcpkg\scripts\buildsystems\vcpkg.cmake
-cmake --build vs_studio_build 
+cmake -B vs_studio_build -S . -A=x64 -DCMAKE_TOOLCHAIN_FILE=d:\dev\trading\vcpkg\scripts\buildsystems\vcpkg.cmake
+cmake --build vs_studio_build --config Debug
 
